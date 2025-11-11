d33b0e72c72b29c80ff4ffc75f34a63abc751485
danny437 <152043385+danny437@users.noreply.github.com>
2025-11-11 08:32:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,12 @@ def buscar():
 # =========================
 # CONFIGURACIÓN Y EJECUCIÓN
 # =========================
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
 
 
