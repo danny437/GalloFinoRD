6c13e330bb03076164ce9d8afa0dfc64eb8d5af6
danny437 <152043385+danny437@users.noreply.github.com>
2025-11-24 19:22:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1533,7 +1533,7 @@ init(); animate();
 @proteger_ruta
 def exportar():
     traba = session['traba']
-    conn = sqlite3.connect(DB)
+    conn = sqlite3.connect(DB
     conn.row_factory = sqlite3.Row
     ...
     cursor = conn.cursor()
@@ -2082,6 +2082,7 @@ if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
 
